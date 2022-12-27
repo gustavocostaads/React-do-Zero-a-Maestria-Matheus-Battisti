fb86fde7b4de080877c0cf02e19c1f84023c2c56
gcrsanalista <gcrsanalista@gmail.com>
2022-12-26 22:31:43 -0300

    miniblog

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes></Routes>
+        </div>
+      </BrowserRouter>
       <h1>Miniblog</h1>
     </div>
   );
---
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASErDb1qRcYWPJXycBgVXaxwdXkRD4l1o",
+  authDomain: "miniblog-8f0c2.firebaseapp.com",
+  projectId: "miniblog-8f0c2",
+  storageBucket: "miniblog-8f0c2.appspot.com",
+  messagingSenderId: "713021767071",
+  appId: "1:713021767071:web:f2fceed15dc45c088611aa"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
