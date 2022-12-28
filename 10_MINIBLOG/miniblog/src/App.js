964a0921bc6c6a73992f13cb3d579095c9567a17
Gustavo Costa <gustavocostaads@gmail.com>
2022-12-28 13:16:22 +0000

    segue

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+// pages
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" elemente={<Home />} />
+            <Route path="/about" elemente={<About />} />
+          </Routes>
         </div>
       </BrowserRouter>
-      <h1>Miniblog</h1>
     </div>
   );
 }
